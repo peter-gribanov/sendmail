d69f1452f8ac770d8b19b938492fcd39fc89a542
Peter Gribanov <info@peter-gribanov.ru>
2015-10-08 17:01:40 +0300

    test smtp exception

---
+++ tests/Sender/Smtp/ExceptionTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Sendmail package
+ *
+ * @package   Sendmail
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2010, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Sendmail\Tests\Sender\Smtp;
+
+use Sendmail\Sender\Smtp\Exception;
+
+/**
+ * @package Sendmail\Tests\Sender\Smtp
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Dialogue
+     *
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dialogue;
+
+    /**
+     * (non-PHPdoc)
+     * @see PHPUnit_Framework_TestCase::setUp()
+     */
+    protected function setUp()
+    {
+        $this->dialogue = $this
+            ->getMockBuilder('\Sendmail\Sender\Smtp\Dialogue')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * Get log
+     *
+     * @return array
+     */
+    public function getLogs()
+    {
+        return array(
+            array(
+                "200 OK\n",
+                'OK',
+                200
+            ),
+            array(
+                "404 Not Found\n",
+                'Not Found',
+                404
+            ),
+            array(
+                "1234 Bad status\n".
+                "Some Headers\n",
+                '4 Bad status',
+                123
+            ),
+            array(
+                "No set status",
+                'No set status',
+                500
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider getLogs
+     */
+    public function testConstruct($log, $message, $code)
+    {
+        $this->dialogue
+            ->expects($this->once())
+            ->method('getLog')
+            ->willReturn($log);
+        $exception = new Exception($this->dialogue);
+        $this->assertEquals($code, $exception->getCode());
+        $this->assertEquals($message, $exception->getMessage());
+    }
+
+    public function testGetDialogue()
+    {
+        $exception = new Exception($this->dialogue);
+        $this->assertEquals($this->dialogue, $exception->getDialogue());
+    }
+}
